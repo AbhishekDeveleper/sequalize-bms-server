c1d03c62414dc02751bd49bc69df24a34674b5ed
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-02-06 12:50:42 +0530

    chore(server) : configure express server in ts

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+export const app = express();
+
+app.options("*", cors());
+
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request for this IP",
+});
+
+app.use(express.json({ limit: "20kb" }));
+app.use("/api", limiter);
+app.use(morgan('dev'))
+
+
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(
+    `The url is ${req.originalUrl} and the method  of this url is ${req.method}`
+  );
+  next();
+});
+
+app.get("/home", (req: Request, res:Response) => {
+  res.send("Welcome to home Page ...");
+});
+
+
+app.use("*", (req, res, next) => {
+  res.send(`The route with this url ${req.originalUrl} is not defined`);
+  next();
+});
+
