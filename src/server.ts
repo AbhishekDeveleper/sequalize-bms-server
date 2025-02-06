589c485c1e4972ef6a5aac0c14a4ca9fc93f43fa
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-02-06 12:51:23 +0530

    chore(server) : express server and cluster to use all core of cpu

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import cluster from "node:cluster";
+import numCPUs from "node:os";
+import process from "node:process";
+import { app } from "./app.js";
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCPUs.availableParallelism(); i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `worker with process id ${process.pid} is died bysignal ${signal}`
+    );
+  });
+} else {
+  const server = app.listen(3000, () => {
+    console.log(
+      `server listening on port 3000 with process id => ${process.pid}`
+    );
+  });
+  process.on(
+    "unhandledRejection",
+    (error: { name?: string; message?: string }) => {
+      console.log(`Unhandled rejection  🎇 shutting down system`);
+      console.log(error);
+      server.close(() => {
+        process.exit(1);
+      });
+    }
+  );
+}
