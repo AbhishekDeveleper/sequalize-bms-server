b8d994f8328855994887e52485cb4b867ef7026d
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-02-10 14:23:23 +0530

    add : add file for making database connection

---
+++ build/config/database.js
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+export const sequelize = new Sequelize("bms_db", "abhishek", "password", {
+    host: "localhost",
+    dialect: "mysql",
+});
+export const connectDb = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    }
+    catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
