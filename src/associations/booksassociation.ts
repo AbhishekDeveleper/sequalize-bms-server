1e977ba2a31d4a362788f28c3ebb85195d5c5e89
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-02-10 14:21:56 +0530

    feature : add new association rule for book gener and author

---
+++ src/associations/booksassociation.ts
@@ -0,0 +1,21 @@
+import { bookModel, authorModel, genreModel } from "../models/bmsModel.js";
+genreModel.hasOne(bookModel, {
+  foreignKey: "generId",
+  as: "book",
+});
+
+bookModel.belongsTo(genreModel, {
+  foreignKey: "generId",
+  as: "category",
+});
+
+authorModel.hasMany(bookModel, {
+  foreignKey: "authorId",
+  as: "books",
+});
+bookModel.belongsTo(authorModel, {
+  foreignKey: "authorId",
+  as: "author",
+});
+
+export { bookModel, authorModel, genreModel };
