fddeb0a28722c50478dd857f832c4cd11ef962a0
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-04-17 13:24:01 +0530

    add(route) : add route for file processing

--- src/routes/bmsRoutes.ts
+++ src/routes/bmsRoutes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import { addBook, deleteBookById, getBook, updateBookWithId } from "../controller/bmsController.js";
+import { addBook, deleteBookById, fileProcessingWithStream, getBook, updateBookWithId } from "../controller/bmsController.js";
 const router = express.Router();
-console.log('runs before')
 router.post("/books", addBook);
+router.get("/process", fileProcessingWithStream);
 router.get("/books", getBook);
 router.delete("/books/:id", deleteBookById)
 router.patch('/books/:id',updateBookWithId)
