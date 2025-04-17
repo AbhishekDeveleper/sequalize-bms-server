495a59c2c74c31e4fd3a012aa0c6011dcda3af98
AbhishekDeveleper <abhsiheksingh55568@gmail.com>
2025-04-17 13:30:23 +0530

    feat(event) : add event emitter

---
+++ build/controller/bookEvent.js
@@ -0,0 +1,17 @@
+import EventEmitter from "events";
+class BookEvents extends EventEmitter {
+    constructor() {
+        super();
+        this.setupEventListeners();
+    }
+    setupEventListeners() {
+        this.on("book_created", (book) => {
+            console.log(`New book : ${book.bookTitle} added by ${book.authorName} of ${book.bookPrice}$ `);
+        });
+        // Error handlers
+        this.on("error", (error) => {
+            console.error("Book system error:", error);
+        });
+    }
+}
+export default BookEvents;
